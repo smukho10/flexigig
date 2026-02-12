c472a7d30ceee3c2ae2e0687266c078ead354017
tgautam2 <tgautam2@asu.edu>
2026-02-12 02:40:39 -0700

    Remove server running log from server.js
    Removed server listen log statement.

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,8 +11,6 @@ const passport = require('./config/passport.js');
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 /**
  * CORS
  * - Allow localhost dev
